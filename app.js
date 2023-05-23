8145ba27149fa8bb8b8c60be0e759179bb3652f5
lulla-by <thfl3363@hanmail.net>
2023-05-23 23:46:54 +0900

    saving usename - localStorage

--- app.js
+++ app.js
@@ -3,17 +3,30 @@ const loginInput = document.querySelector("#loginForm input");
 const greeting = document.querySelector("#greeting");
 
 const HIDDEN_CLASSNAME = "hidden"
+const USERNAME = "username"
 
 const onSubmit = (e) => {
   e.preventDefault();
   const userName = loginInput.value;
+  localStorage.setItem(USERNAME,userName)
   loginForm.classList.add(HIDDEN_CLASSNAME)
-  greeting.classList.remove(HIDDEN_CLASSNAME)
-  greeting.innerText = `Hello ${userName}`
+  paintGreetings(userName)
 };
 
+const savedUsername = localStorage.getItem(USERNAME)
 
+const paintGreetings = (username)=>{
+  greeting.innerText = `Hello ${username}`
+  greeting.classList.remove(HIDDEN_CLASSNAME);
+}
 
+if(savedUsername === null){
+loginForm.classList.remove(HIDDEN_CLASSNAME)
 loginForm.addEventListener("submit", onSubmit);
+}else{
+  paintGreetings(savedUsername)
+}
+
+
  
 
