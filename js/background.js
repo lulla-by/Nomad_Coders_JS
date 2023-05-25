1df6d1c429d5ae5d5d9f98ee067ff590a74722cc
lulla-by <thfl3363@hanmail.net>
2023-05-25 13:36:24 +0900

    Quotes And Background

---
+++ js/background.js
@@ -0,0 +1,10 @@
+const images = ["0.jpg", "1.jpg", "2.jpg"];
+const choosenImage = images[Math.floor(Math.random() * images.length)]
+
+
+const image = document.createElement("img")
+
+image.src = `img/${choosenImage}`
+
+document.body.appendChild(image)
+console.log(image)
